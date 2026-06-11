815c5d0b4e143d74847f57b48dd8093d867bc6a5
Eddie-dev1 <eduardoficial18@gmail.com>
2026-06-11 17:00:57 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+
+    private int matricula;
+    private String nome;
+    private String email;
+
+    public Usuario(int matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
